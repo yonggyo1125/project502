4bf9365b73de10a6c23f565c92f818fa6cee653e
yonggyo1125 <yonggyo00@kakao.com>
2023-12-29 15:00:35 +0900

    셋팅 작업중

---
+++ src/main/java/org/choongang/commons/rests/JSONData.java
@@ -0,0 +1,4 @@
+package org.choongang.commons.rests;
+
+public class JSONData<T> {
+}
---
+++ src/main/java/org/choongang/restcontrollers/RestCommonController.java
@@ -0,0 +1,9 @@
+package org.choongang.restcontrollers;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice("org.choongang.restcontrollers")
+public class RestCommonController {
+
+
+}
