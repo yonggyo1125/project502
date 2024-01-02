3a9237a655c682385c6c827fbdec8ca44954b5a5
yonggyo1125 <yonggyo00@kakao.com>
2024-01-02 15:47:29 +0900

    회원 기능

---
+++ src/main/java/org/choongang/member/MemberUtil.java
@@ -0,0 +1,15 @@
+package org.choongang.member;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberUtil {
+
+    public static void clearLoginData(HttpSession session) {
+        session.removeAttribute("username");
+        session.removeAttribute("NotBlank_username");
+        session.removeAttribute("NotBlank_password");
+    }
+
+}
--- src/main/java/org/choongang/member/service/LoginFailureHandler.java
+++ src/main/java/org/choongang/member/service/LoginFailureHandler.java
@@ -19,6 +19,8 @@ public class LoginFailureHandler implements AuthenticationFailureHandler {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
+        session.setAttribute("username", username);
+
         if (!StringUtils.hasText(username)) {
             session.setAttribute("NotBlank_username", Utils.getMessage("NotBlank.userId"));
         }
