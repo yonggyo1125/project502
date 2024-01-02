f898ce3ff95be5b78d5fb453ea6dfc0b398d9682
yonggyo1125 <yonggyo00@kakao.com>
2024-01-02 16:00:56 +0900

    회원 기능

--- src/main/java/org/choongang/member/service/LoginSuccessHandler.java
+++ src/main/java/org/choongang/member/service/LoginSuccessHandler.java
@@ -3,6 +3,8 @@ package org.choongang.member.service;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.choongang.member.MemberUtil;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -11,6 +13,7 @@ import java.io.IOException;
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-
+        HttpSession session = request.getSession();
+        MemberUtil.clearLoginData(session);
     }
 }
