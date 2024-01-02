6015f8de162d00c273b966577550f7938c2bd6a6
yonggyo1125 <yonggyo00@kakao.com>
2024-01-02 09:47:57 +0900

    회원 기능

---
+++ src/main/java/org/choongang/member/service/JoinService.java
@@ -0,0 +1,13 @@
+package org.choongang.member.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JoinService {
+
+    public void process() {
+
+    }
+}
