0161f28d348e0d7cceddb1bde7fb3fe4d270cd3e
yonggyo1125 <yonggyo00@kakao.com>
2024-01-02 12:45:09 +0900

    회원 기능

--- src/main/java/org/choongang/member/controllers/JoinValidator.java
+++ src/main/java/org/choongang/member/controllers/JoinValidator.java
@@ -1,6 +1,7 @@
 package org.choongang.member.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.commons.validators.PasswordValidator;
 import org.choongang.member.repositories.MemberRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -9,7 +10,7 @@ import org.springframework.validation.Validator;
 
 @Component
 @RequiredArgsConstructor
-public class JoinValidator implements Validator {
+public class JoinValidator implements Validator, PasswordValidator {
 
     private final MemberRepository memberRepository;
 
@@ -42,6 +43,10 @@ public class JoinValidator implements Validator {
         }
 
         // 2. 비밀번호 복잡성 체크 - 대소문자 1개 각각 포함, 숫자 1개 이상 포함, 특수문자도 1개 이상 포함
+        if (StringUtils.hasText(password) &&
+                (!alphaCheck(password, true) || !numberCheck(password) || !specialCharsCheck(password))) {
+            errors.rejectValue("password", "Complexity");
+        }
 
         // 3. 비밀번호, 비밀번호 확인 일치 여부 체크
         if (StringUtils.hasText(password) && StringUtils.hasText(confirmPassword)
--- src/main/java/org/choongang/member/service/JoinService.java
+++ src/main/java/org/choongang/member/service/JoinService.java
@@ -1,13 +1,39 @@
 package org.choongang.member.service;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.member.controllers.JoinValidator;
+import org.choongang.member.controllers.RequestJoin;
+import org.choongang.member.entities.Member;
+import org.choongang.member.repositories.MemberRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.Errors;
 
 @Service
 @RequiredArgsConstructor
 public class JoinService {
 
-    public void process() {
+    private final MemberRepository memberRepository;
+    private final JoinValidator validator;
+    private final PasswordEncoder encoder;
 
+    public void process(RequestJoin form, Errors errors) {
+        validator.validate(form, errors);
+        if (errors.hasErrors()) {
+            return;
+        }
+
+        // 비밀번호 BCrypt로 해시화
+        String hash = encoder.encode(form.getPassword());
+
+        Member member = new ModelMapper().map(form, Member.class);
+        member.setPassword(hash);
+
+        process(member);
+    }
+
+    public void process(Member member) {
+        memberRepository.saveAndFlush(member);
     }
 }
