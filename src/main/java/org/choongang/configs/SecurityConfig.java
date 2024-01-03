3747b7148857e98136d97829f3240f0312aae1e9
yonggyo1125 <yonggyo00@kakao.com>
2024-01-03 11:00:41 +0900

    회원 기능

--- src/main/java/org/choongang/configs/SecurityConfig.java
+++ src/main/java/org/choongang/configs/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.choongang.configs;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.choongang.member.service.LoginFailureHandler;
 import org.choongang.member.service.LoginSuccessHandler;
 import org.springframework.context.annotation.Bean;
@@ -31,8 +32,32 @@ public class SecurityConfig {
         });
         /* 인증 설정 E - 로그인, 로그아웃 */
         
-        
-        
+        /* 인가 설정 S - 접근 통제 */
+        // hasAuthority(..) hasAnyAuthority(...), hasRole, hasAnyRole
+        // ROLE_롤명칭
+        // hasAuthority('ADMIN')
+        // ROLE_ADMIN -> hasAuthority('ROLE_ADMIN')
+        // hasRole('ADMIN')
+        http.authorizeHttpRequests(c -> {
+           c.requestMatchers("/mypage/**").authenticated() // 회원 전용
+                   .requestMatchers("/admin/**").hasAnyAuthority("ADMIN", "MANAGER")
+                   .anyRequest().permitAll(); // 그외 모든 페이지는 모두 접근 가능
+        });
+
+        http.exceptionHandling(c -> {
+           c.authenticationEntryPoint((req, res, e) -> {
+                String URL = req.getRequestURI();
+                if (URL.indexOf("/admin") != -1) { // 관리자 페이지
+                    res.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+                } else { // 회원전용 페이지
+                    res.sendRedirect(req.getContextPath() + "/member/login");
+                }
+
+           });
+        });
+
+        /* 인가 설정 E - 접근 통제 */
+
         return http.build();
     }
 
--- src/test/java/org/choongang/ProjectApplicationTests.java
+++ src/test/java/org/choongang/ProjectApplicationTests.java
@@ -2,10 +2,8 @@ package org.choongang;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = "spring.profiles.active=test")
 class ProjectApplicationTests {
 
 	@Test
