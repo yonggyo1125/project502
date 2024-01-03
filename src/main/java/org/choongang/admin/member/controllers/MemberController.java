dccc81019d70e065a14ee58e705725abb194ade4
yonggyo1125 <yonggyo00@kakao.com>
2024-01-03 17:00:35 +0900

    관리자 구성

--- src/main/java/org/choongang/admin/member/controllers/MemberController.java
+++ src/main/java/org/choongang/admin/member/controllers/MemberController.java
@@ -4,6 +4,7 @@ import org.choongang.admin.menus.Menu;
 import org.choongang.admin.menus.MenuDetail;
 import org.choongang.commons.ExceptionProcessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,11 @@ import java.util.List;
 @RequestMapping("/admin/member")
 public class MemberController implements ExceptionProcessor {
 
+    @ModelAttribute("menuCode")
+    public String getMenuCode() {
+        return "member";
+    }
+
     @ModelAttribute("subMenus")
     public List<MenuDetail> getSubMenus() {
 
@@ -21,8 +27,9 @@ public class MemberController implements ExceptionProcessor {
     }
 
     @GetMapping
-    public String list() {
+    public String list(Model model) {
 
+        model.addAttribute("subMenuCode", "list");
         return "admin/member/list";
     }
 }
