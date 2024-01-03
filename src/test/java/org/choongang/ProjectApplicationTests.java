82eadc3fc40621a13dc571293c393bb01a0c1b04
yonggyo1125 <yonggyo00@kakao.com>
2024-01-03 11:26:35 +0900

    회원 기능

--- src/test/java/org/choongang/ProjectApplicationTests.java
+++ src/test/java/org/choongang/ProjectApplicationTests.java
@@ -5,6 +5,7 @@ import org.choongang.member.entities.Authorities;
 import org.choongang.member.entities.Member;
 import org.choongang.member.repositories.AuthoritiesRepository;
 import org.choongang.member.repositories.MemberRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,7 +19,7 @@ class ProjectApplicationTests {
 	@Autowired
 	private AuthoritiesRepository authoritiesRepository;
 
-	@Test
+	@Test @Disabled
 	void contextLoads() {
 		Member member = memberRepository.findByUserId("user01").orElse(null);
 
