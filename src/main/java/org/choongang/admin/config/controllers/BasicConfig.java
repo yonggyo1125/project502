052f9842812640a44dc6da3e9e6df1640b13eab4
yonggyo1125 <yonggyo00@kakao.com>
2024-01-04 15:23:31 +0900

    관리자 구성

--- src/main/java/org/choongang/admin/config/controllers/BasicConfig.java
+++ src/main/java/org/choongang/admin/config/controllers/BasicConfig.java
@@ -4,9 +4,9 @@ import lombok.Data;
 
 @Data
 public class BasicConfig {
-    private String siteTitle;
-    private String siteDescription;
-    private String siteKeywords;
-    private int cssJsVersion;
-    private String joinTerms;
+    private String siteTitle = "";
+    private String siteDescription = "";
+    private String siteKeywords = "";
+    private int cssJsVersion = 1;
+    private String joinTerms = "";
 }
