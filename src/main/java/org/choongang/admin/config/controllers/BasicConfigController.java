a8717e4822693212dc531f6ef56da8fa9aa532a0
yonggyo1125 <yonggyo00@kakao.com>
2024-01-04 09:46:32 +0900

    관리자 구성

--- src/main/java/org/choongang/admin/config/controllers/BasicConfigController.java
+++ src/main/java/org/choongang/admin/config/controllers/BasicConfigController.java
@@ -3,6 +3,7 @@ package org.choongang.admin.config.controllers;
 import lombok.RequiredArgsConstructor;
 import org.choongang.commons.ExceptionProcessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,12 +20,12 @@ public class BasicConfigController implements ExceptionProcessor {
     }
 
     @GetMapping
-    public String index() {
+    public String index(@ModelAttribute BasicConfig config, Model model) {
         return "admin/config/basic";
     }
 
     @PostMapping
-    public String save() {
+    public String save(BasicConfig config, Model model) {
 
         return "admin/config/basic";
     }
