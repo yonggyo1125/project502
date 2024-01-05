582c0f4ba725bd93d2783cfe6e516532747359a1
yonggyo1125 <yonggyo00@kakao.com>
2024-01-05 16:00:58 +0900

    파일 공통 기능 구현

--- src/main/java/org/choongang/file/service/FileDeleteService.java
+++ src/main/java/org/choongang/file/service/FileDeleteService.java
@@ -1,7 +1,9 @@
 package org.choongang.file.service;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.file.entities.FileInfo;
 import org.choongang.member.MemberUtil;
+import org.choongang.member.entities.Member;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +14,14 @@ public class FileDeleteService {
     private final MemberUtil memberUtil;
 
     public void delete(Long seq) {
+        FileInfo data = infoService.get(seq);
+
+        // 파일 삭제 권한 체크
+        Member member = memberUtil.getMember();
+
+        if (!memberUtil.isAdmin()) {
+
+        }
 
     }
 }
