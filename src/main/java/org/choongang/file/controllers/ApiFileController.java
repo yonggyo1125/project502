1fc38cb11fb7c09df496bd42a20332deb1d298ff
yonggyo1125 <yonggyo00@kakao.com>
2024-01-05 10:57:38 +0900

    파일 공통 기능 구현

--- src/main/java/org/choongang/file/controllers/ApiFileController.java
+++ src/main/java/org/choongang/file/controllers/ApiFileController.java
@@ -2,12 +2,16 @@ package org.choongang.file.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.choongang.commons.ExceptionRestProcessor;
+import org.choongang.commons.rests.JSONData;
+import org.choongang.file.entities.FileInfo;
 import org.choongang.file.service.FileUploadService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/file")
@@ -17,11 +21,11 @@ public class ApiFileController implements ExceptionRestProcessor {
     private final FileUploadService uploadService;
 
     @PostMapping
-    public void upload(@RequestParam("file") MultipartFile[] files,
-                       @RequestParam(name="gid", required = false) String gid,
-                       @RequestParam(name="location", required = false) String location) {
+    public JSONData<List<FileInfo>> upload(@RequestParam("file") MultipartFile[] files,
+                                           @RequestParam(name="gid", required = false) String gid,
+                                           @RequestParam(name="location", required = false) String location) {
 
-        uploadService.upload(files, gid, location);
+        List<FileInfo> uploadedFiles = uploadService.upload(files, gid, location);
 
     }
 }
