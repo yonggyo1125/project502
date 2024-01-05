ef3abb25acc20175440946279e59713a128bff0c
yonggyo1125 <yonggyo00@kakao.com>
2024-01-05 10:47:23 +0900

    파일 공통 기능 구현

--- src/main/java/org/choongang/file/controllers/ApiFileController.java
+++ src/main/java/org/choongang/file/controllers/ApiFileController.java
@@ -1,6 +1,8 @@
 package org.choongang.file.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.choongang.commons.ExceptionRestProcessor;
+import org.choongang.file.service.FileUploadService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,10 +10,16 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/file")
+@RequiredArgsConstructor
 public class ApiFileController implements ExceptionRestProcessor {
+
+    private final FileUploadService uploadService;
+
     public void upload(@RequestParam("file") MultipartFile[] files,
                        @RequestParam(name="gid", required = false) String gid,
                        @RequestParam(name="location", required = false) String location) {
+
+        uploadService.upload(files, gid, location);
 
     }
 }
