71c40f6c7092703e991113d7b1edcb37d5c33985
yonggyo1125 <yonggyo00@kakao.com>
2024-01-05 10:34:35 +0900

    파일 공통 기능 구현

--- src/main/java/org/choongang/file/entities/FileInfo.java
+++ src/main/java/org/choongang/file/entities/FileInfo.java
@@ -33,5 +33,8 @@ public class FileInfo extends BaseMember {
     @Column(length=30)
     private String extension;
 
+    @Column(length=65)
+    private String fileType;
+
     private boolean done;
 }
--- src/main/java/org/choongang/file/service/FileUploadService.java
+++ src/main/java/org/choongang/file/service/FileUploadService.java
@@ -1,9 +1,56 @@
 package org.choongang.file.service;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.configs.FileProperties;
+import org.choongang.file.entities.FileInfo;
+import org.choongang.file.repositories.FileInfoRepository;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@EnableConfigurationProperties(FileProperties.class)
 public class FileUploadService {
+
+    private final FileProperties fileProperties;
+    private final FileInfoRepository repository;
+
+    public List<FileInfo> upload(MultipartFile[] files, String gid, String location) {
+        /**
+         * 1. 파일 정보 저장
+         * 2. 서버쪽에 파일 업로드 처리
+         */
+
+        gid = StringUtils.hasText(gid) ? gid : UUID.randomUUID().toString();
+
+        String uploadPath = fileProperties.getPath(); // 파일 업로드 기본 경로
+
+        for (MultipartFile file : files) {
+            /* 파일 정보 저장 S */
+            String fileName = file.getOriginalFilename(); // 업로드시 원 파일명
+            // 파일명.확장자   image.png,  image.1.png
+
+            // 확장자
+            String extension = fileName.substring(fileName.lastIndexOf("."));
+
+            String fileType = file.getContentType(); // 파일 종류 - 예) image/..
+
+            FileInfo fileInfo = FileInfo.builder()
+                    .gid(gid)
+                    .location(location)
+                    .fileName(fileName)
+                    .extension(extension)
+                    .fileType(fileType)
+                    .build();
+
+            repository.saveAndFlush(fileInfo);
+            /* 파일 정보 저장 E */
+        }
+
+    }
 }
