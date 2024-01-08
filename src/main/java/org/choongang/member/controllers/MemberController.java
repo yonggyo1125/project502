270d4fba0d0feff76f71b33f7fb04d346891b0dc
yonggyo1125 <yonggyo00@kakao.com>
2024-01-08 12:39:17 +0900

    파일 공통 기능 구현

--- src/main/java/org/choongang/member/controllers/MemberController.java
+++ src/main/java/org/choongang/member/controllers/MemberController.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/member")
 @RequiredArgsConstructor
@@ -53,13 +56,17 @@ public class MemberController implements ExceptionProcessor {
     private void commonProcess(String mode, Model model) {
         mode = StringUtils.hasText(mode) ? mode : "join";
         String pageTitle = Utils.getMessage("회원가입", "commons");
+
+        List<String> addCommonScript = new ArrayList<>(); // 공통 자바스크립트
+
         if (mode.equals("login")) {
             pageTitle = Utils.getMessage("로그인", "commons");
 
         } else if (mode.equals("join")) {
-
+            addCommonScript.add("fileManager");
         }
 
         model.addAttribute("pageTitle", pageTitle);
+        model.addAttribute("addCommonScript", addCommonScript);
     }
 }
---
+++ src/main/resources/static/common/js/common.js
@@ -0,0 +1,36 @@
+var commonLib = commonLib || {};
+
+/**
+* ajax 요청,응답 편의 함수
+*
+* @param method : 요청 방식 - GET, POST, PUT, PATCH, DELETE ...
+* @param url : 요청 URL
+* @param params : 요청 데이터(POST, PUT, PATCH ... )
+* @param responseType :  json : javascript 객체로 변환
+*/
+commobLib.ajaxLoad = function(method, url, params, responseType) {
+    method = method || "GET";
+    params = params || null;
+
+    return new Promise((resolve, reject) => {
+        const xhr = XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.send(params); // 요청 body에 실릴 데이터 키=값&키=값& .... FormData 객체 (POST, PATCH, PUT)
+
+        xhr.onreadystatechange = function() {
+            if (xhr.status == 200 && xhr.readyState == XMLHttpRequest.DONE) {
+                const resData = (responseType && responseType.toLowerCase() == 'json') ? JSON.parse(xhr.responseText) : xhr.responseText;
+
+                resolve(resData); // 성공시 응답 데이터
+            }
+        };
+
+        xhr.onabort = function(err) {
+            reject(err); // 중단 시
+        };
+
+        xhr.onerror = function(err) {
+            reject(err); // 요청 또는 응답시 오류 발생
+        };
+    });
+};
