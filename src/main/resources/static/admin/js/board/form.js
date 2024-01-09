8f0806f9efd8a8c5c117e5a8985379323f900489
yonggyo1125 <yonggyo00@kakao.com>
2024-01-09 15:58:31 +0900

    수업 소스

--- src/main/resources/static/admin/js/board/form.js
+++ src/main/resources/static/admin/js/board/form.js
@@ -20,10 +20,27 @@ function callbackFileUpload(files) {
         return;
     }
 
-
+    const tpl = document.getElementById("editor_tpl").innerHTML;
+    const domParser = new DOMParser();
+    const targetTop = document.getElementById("uploaded_files_html_top");
+    const targetBottom = document.getElementById("uploaded_files_html_bottom");
 
     for (const file of files) {
        const editor = file.location == 'html_bottom' ? editor2 : editor1;
+       const target = file.location == 'html_bottom' ? targetBottom : targetTop;
+
        editor.execute('insertImage', { source: file.fileUrl });
+
+       /* 템플릿 데이터 치환 S */
+        let html = tpl;
+        html = html.replace(/\[seq\]/g, file.seq)
+                    .replace(/\[fileName\]/g, file.fileName)
+                    .replace(/\[imageUrl\]/g, file.fileUrl);
+
+        const dom = domParser.parseFromString(html, "text/html");
+        const fileBox = dom.querySelector(".file_tpl_box");
+        target.appendChild(fileBox);
+
+       /* 템플릿 데이터 치환 E */
     }
 }
