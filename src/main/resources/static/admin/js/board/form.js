9a50345c89bdd8452abb5d150670ab653808d7e2
yonggyo1125 <yonggyo00@kakao.com>
2024-01-09 15:44:00 +0900

    수업 소스

--- src/main/resources/static/admin/js/board/form.js
+++ src/main/resources/static/admin/js/board/form.js
@@ -1,7 +1,27 @@
 window.addEventListener("DOMContentLoaded", function() {
     const { loadEditor } = commonLib;
 
-    loadEditor("html_top");
 
-    loadEditor("html_bottom");
+    loadEditor("html_top")
+        .then(editor => window.editor1 = editor)
+        .catch(err => console.error(err));
+
+    loadEditor("html_bottom")
+        .then(editor => window.editor2 = editor)
+        .catch(err => console.error(err));
 });
+
+/**
+* 파일 업로드 후 후속처리 함수
+*
+*/
+function callbackFileUpload(files) {
+    if (!files || files.length == 0) {
+        return;
+    }
+
+    for (const file of files) {
+       const editor = file.location == 'html_bottom' ? editor2 : editor1;
+       editor.execute('insertImage', { source: file.fileUrl });
+    }
+}
