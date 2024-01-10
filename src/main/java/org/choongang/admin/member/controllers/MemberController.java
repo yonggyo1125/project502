fa9345728df4fee19c0bf36bd59db7d74389cbc1
yonggyo1125 <yonggyo00@kakao.com>
2024-01-10 12:21:45 +0900

    수업 소스

--- src/main/java/org/choongang/admin/member/controllers/MemberController.java
+++ src/main/java/org/choongang/admin/member/controllers/MemberController.java
@@ -1,8 +1,13 @@
 package org.choongang.admin.member.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.choongang.admin.menus.Menu;
 import org.choongang.admin.menus.MenuDetail;
 import org.choongang.commons.ExceptionProcessor;
+import org.choongang.commons.ListData;
+import org.choongang.member.controllers.MemberSearch;
+import org.choongang.member.entities.Member;
+import org.choongang.member.service.MemberInfoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +18,10 @@ import java.util.List;
 
 @Controller("adminMemberController")
 @RequestMapping("/admin/member")
+@RequiredArgsConstructor
 public class MemberController implements ExceptionProcessor {
+
+    private final MemberInfoService infoService;
 
     @ModelAttribute("menuCode")
     public String getMenuCode() {
@@ -27,7 +35,12 @@ public class MemberController implements ExceptionProcessor {
     }
 
     @GetMapping
-    public String list(Model model) {
+    public String list(@ModelAttribute MemberSearch search, Model model) {
+
+        ListData<Member> data = infoService.getList(search);
+
+        model.addAttribute("items", data.getItems()); // 목록
+        model.addAttribute("pagination", data.getPagination()); // 페이징
 
         model.addAttribute("subMenuCode", "list");
         return "admin/member/list";
