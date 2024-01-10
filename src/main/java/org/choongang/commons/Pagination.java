18b52f23529283feed36faec8d4165c39e0bac64
yonggyo1125 <yonggyo00@kakao.com>
2024-01-10 10:27:10 +0900

    수업 소스

--- src/main/java/org/choongang/commons/Pagination.java
+++ src/main/java/org/choongang/commons/Pagination.java
@@ -1,11 +1,20 @@
 package org.choongang.commons;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Pagination {
 
     private int page;
     private int total;
     private int ranges;
     private int limit;
+
+    private int firstRangePage; // 구간별 첫 페이지
+    private int lastRangePage; // 구간별 마지막 페이지
+
+    private int prevRangePage; // 이전 구간 첫 페이지 번호
+    private int nextRangePage; // 다음 구간 첫 페이지 번호
 
     /**
      *
@@ -25,15 +34,36 @@ public class Pagination {
         int totalPages = (int)Math.ceil(total / (double)limit);
 
 
-
         // 구간 번호
         int rangeCnt = (page - 1) / ranges;
         int firstRangePage = rangeCnt * ranges + 1;
         int lastRangePage = firstRangePage + ranges - 1;
+        lastRangePage = lastRangePage > totalPages ? totalPages : lastRangePage;
+
+        // 이전 구간 첫 페이지
+        if (rangeCnt > 0) {
+            prevRangePage = firstRangePage - ranges;
+        }
+
+        // 다음 구간 첫 페이지
+        // 마지막 구간 번호
+        int lastRangeCnt = (totalPages - 1) / ranges;
+        if (rangeCnt < lastRangeCnt) { // 마지막 구간이 아닌 경우 다음 구간 첫 페이지 계산
+            nextRangePage = firstRangePage + ranges;
+        }
 
         this.page = page;
         this.total = total;
         this.ranges = ranges;
         this.limit = limit;
+        this.prevRangePage = firstRangePage;
+        this.nextRangePage = lastRangePage;
+    }
+
+    public List<String[]> getPages() {
+        // 0 : 페이지 번호, 1 : 페이지 URL - ?page=페이지번호
+
+        List<String[]> data = IntStream.rangeClosed()
+
     }
 }
