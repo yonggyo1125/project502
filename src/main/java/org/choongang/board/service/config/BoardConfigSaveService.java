74cf009e9a100a523aee72fdebcaa32941ab4e8c
yonggyo1125 <yonggyo00@kakao.com>
2024-01-11 14:31:46 +0900

    4조 소스

---
+++ src/main/java/org/choongang/board/service/config/BoardConfigSaveService.java
@@ -0,0 +1,10 @@
+package org.choongang.board.service.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardConfigSaveService {
+
+}
