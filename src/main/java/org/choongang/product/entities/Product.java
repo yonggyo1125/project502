f031b8ad26e223c057f03410458953292ebf6e52
yonggyo1125 <yonggyo00@kakao.com>
2024-01-12 15:32:16 +0900

    1조 소스

--- src/main/java/org/choongang/product/entities/Product.java
+++ src/main/java/org/choongang/product/entities/Product.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.choongang.commons.entities.Base;
+import org.choongang.product.constants.DiscountType;
+import org.choongang.product.constants.ProductStatus;
 
 @Entity
 @Data @Builder
@@ -24,7 +26,28 @@ public class Product extends Base {
     private int consumerPrice; // 소비자가(보이는 금액)
     private int salePrice; // 판매가(결제 기준 금액)
 
+    private boolean useStock; // 재고 사용 여부 - true : 재고 차감
     private int stock; // 옵션을 사용하지 않는 경우 단일 상품 재고, 0 - 무제한
 
+    @Enumerated(EnumType.STRING)
+    @Column(length=10, nullable = false)
+    private DiscountType discountType = DiscountType.PERCENT;
+    private int discount; // 할인 금액
 
+    @Lob
+    private String extraInfo; // 상품 추가 정보 : JSON 문자열로 저장
+
+    private boolean packageDelivery; // 같은 판매자별 묶음 배송 여부
+    private int deliveryPrice; // 배송비, 0이면 무료 배송
+
+    @Lob
+    private String description; // 상품 상세 설명
+
+    private float score; // 평점
+
+    private boolean active; // 노출 여부 : true -> 소비자 페이지 노출
+
+    @Enumerated(EnumType.STRING)
+    @Column(length=15, nullable = false)
+    private ProductStatus status = ProductStatus.PREPARE; // 상품 상태
 }
