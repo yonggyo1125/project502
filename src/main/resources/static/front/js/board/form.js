2eb0b85838fc305c8f899332c7a8ae1711c57266
yonggyo1125 <yonggyo00@kakao.com>
2024-01-15 15:54:00 +0900

    게시판

---
+++ src/main/resources/static/front/js/board/form.js
@@ -0,0 +1,65 @@
+window.addEventListener("DOMContentLoaded", function() {
+    if (typeof ClassicEditor == 'function') { // 에디터 사용
+        const { loadEditor } = commonLib;
+
+        loadEditor("content", 450)
+            .then(editor => window.editor = editor);
+    } // 에디터 사용 E
+});
+
+
+// 에디터에 이미지 추가
+function insertImage(source) {
+    editor.execute('insertImage', { source })
+}
+
+/**
+* 파일 업로드 후속 처리
+*
+*/
+function callbackFileUpload(files) {
+    if (!files || files.length == 0) {
+        return;
+    }
+
+    const imageUrls = [];
+
+    const editorTpl = document.getElementById("editor_tpl").innerHTML;
+    const attachTpl = document.getElementById("attach_tpl").innerHTML;
+
+    const editorFiles = document.getElementById("editor_files");
+    const attachFiles = document.getElementById("attach_files");
+
+    const domParser = new DOMParser();
+
+    for (const file of files) {
+        const location = file.location;
+
+        let html = location == 'editor' ? editorTpl : attachTpl;
+        const targetEl = location == 'editor' ? editorFiles : attachFiles;
+
+        if (location == 'editor') {
+            imageUrls.push(file.fileUrl);
+        }
+
+        html = html.replace(/\[seq\]/g, file.seq)
+                    .replace(/\[fileName\]/g, file.fileName)
+                    .replace(/\[imageUrl\]/g, file.fileUrl);
+
+        const dom = domParser.parseFromString(html, "text/html");
+        const fileBox = dom.querySelector(".file_tpl_box");
+
+        targetEl.appendChild(fileBox);
+    }
+
+    if (imageUrls.length > 0) insertImage(imageUrls);
+
+}
+
+/**
+* 파일 삭제후 후속 처리
+*
+*/
+function callbackFileDelete(seq) {
+
+}
