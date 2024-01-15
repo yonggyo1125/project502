50b270696caa95831695f7e40835c5705cd069a9
yonggyo1125 <yonggyo00@kakao.com>
2024-01-15 09:34:21 +0900

    게시판

--- src/main/java/org/choongang/commons/Utils.java
+++ src/main/java/org/choongang/commons/Utils.java
@@ -137,4 +137,24 @@ public class Utils {
     public static int onlyPositiveNumber(int num, int replace) {
         return num < 1 ? replace : num;
     }
+
+    /**
+     * 요청 데이터 단일 조회 편의 함수
+     *
+     * @param name
+     * @return
+     */
+    public String getParam(String name) {
+        return request.getParameter(name);
+    }
+
+    /**
+     * 요청 데이터 복수개 조회 편의 함수
+     *
+     * @param name
+     * @return
+     */
+    public String[] getParams(String name) {
+        return request.getParameterValues(name);
+    }
 }
