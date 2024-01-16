bbe36656b369de9825d80edf2cf6448180c6ee8b
yonggyo1125 <yonggyo00@kakao.com>
2024-01-16 10:29:30 +0900

    게시판

--- src/main/java/org/choongang/board/service/BoardInfoService.java
+++ src/main/java/org/choongang/board/service/BoardInfoService.java
@@ -1,9 +1,49 @@
 package org.choongang.board.service;
 
+import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.choongang.board.entities.BoardData;
+import org.choongang.board.repositories.BoardDataRepository;
+import org.choongang.file.entities.FileInfo;
+import org.choongang.file.service.FileInfoService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BoardInfoService {
+
+    private final EntityManager em;
+    private final BoardDataRepository boardDataRepository;
+    private final FileInfoService fileInfoService;
+
+    /**
+     * 게시글 조회
+     *
+     * @param seq : 게시글 번호
+     * @return
+     */
+    public BoardData get(Long seq) {
+        BoardData boardData = boardDataRepository.findById(seq).orElseThrow(BoardDataNotFoundException::new);
+
+        addBoardData(boardData);
+
+        return boardData;
+    }
+
+    /**
+     * 게시글 추가 정보 처리
+     *
+     * @param boardData
+     */
+    public void addBoardData(BoardData boardData) {
+        String gid = boardData.getGid();
+
+        List<FileInfo> editorFiles = fileInfoService.getListDone(gid, "editor");
+        List<FileInfo> attachFiles = fileInfoService.getListDone(gid, "attach");
+
+        boardData.setEditorFiles(editorFiles);
+        boardData.setAttachFiles(attachFiles);
+    }
 }
