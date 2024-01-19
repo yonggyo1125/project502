7984965b49d09d0a4220a984f6ac35aeacc2172c
yonggyo1125 <yonggyo00@kakao.com>
2024-01-19 09:45:55 +0900

    2조!

---
+++ src/main/java/org/choongang/board/controllers/comment/RequestComment.java
@@ -0,0 +1,19 @@
+package org.choongang.board.controllers.comment;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class RequestComment {
+    private String mode = "add";
+
+    private Long seq; // 댓글 등록번호
+
+    @NotBlank
+    private String commenter; // 작성자
+
+    private String guestPw; // 비회원 비밀번호
+
+    @NotBlank
+    private String content; // 댓글 내용
+}
