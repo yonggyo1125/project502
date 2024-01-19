7e8b9824c0ed062dedc36f5ff7837843caf54b4c
yonggyo1125 <yonggyo00@kakao.com>
2024-01-19 09:56:12 +0900

    2조!

--- src/main/java/org/choongang/board/controllers/comment/CommentController.java
+++ src/main/java/org/choongang/board/controllers/comment/CommentController.java
@@ -1,9 +1,16 @@
 package org.choongang.board.controllers.comment;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.choongang.board.service.comment.CommentSaveService;
 import org.choongang.commons.ExceptionProcessor;
+import org.choongang.commons.Utils;
+import org.choongang.commons.exceptions.AlertException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +19,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class CommentController implements ExceptionProcessor {
 
+    private final CommentFormValidator commentFormValidator;
+    private final CommentSaveService commentSaveService;
+
     /**
      * 댓글 저장, 수정 처리
      *
@@ -19,7 +29,19 @@ public class CommentController implements ExceptionProcessor {
      * @return
      */
     @PostMapping("/save")
-    public String save(Model model) {
+    public String save(@Valid RequestComment form, Errors errors, Model model) {
+
+        commonProcess(form.getMode(), model);
+
+        commentFormValidator.validate(form, errors);
+
+        if (errors.hasErrors()) {
+            FieldError error = errors.getFieldErrors().stream().findFirst().orElse(null);
+
+            throw new AlertException(Utils.getMessage(error.getCodes()[0]), HttpStatus.BAD_REQUEST);
+        }
+
+        commentSaveService.save(form); // 댓글 저장, 수정
 
         model.addAttribute("script", "parent.location.reload();");
         return "common/_execute_script";
