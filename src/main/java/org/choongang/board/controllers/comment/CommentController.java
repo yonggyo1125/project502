f97aeb1d0d92e19c79283d19eb3cb182d911de42
yonggyo1125 <yonggyo00@kakao.com>
2024-01-19 12:51:04 +0900

    댓글

--- src/main/java/org/choongang/board/controllers/comment/CommentController.java
+++ src/main/java/org/choongang/board/controllers/comment/CommentController.java
@@ -44,7 +44,9 @@ public class CommentController implements ExceptionProcessor {
 
         CommentData commentData = commentSaveService.save(form); // 댓글 저장, 수정
 
-        model.addAttribute("script", "parent.location.reload();parent.location.hash=#comment_" + commentData.getSeq());
+        String script = String.format("parent.location.replace('/board/view/%d#comment_%d');", commentData.getBoardData().getSeq(), commentData.getSeq());
+
+        model.addAttribute("script", script);
 
         return "common/_execute_script";
     }
