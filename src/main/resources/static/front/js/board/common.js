0e941d454f0064634f92e30d627d3829ef904691
yonggyo1125 <yonggyo00@kakao.com>
2024-01-20 14:45:50 +0900

    3조!

--- src/main/resources/static/front/js/board/common.js
+++ src/main/resources/static/front/js/board/common.js
@@ -7,6 +7,9 @@ const boardLib = {
     *
     */
     save(bSeq) {
+        const { ajaxLoad } = commonLib;
+
+        ajaxLoad('GET', `/api/board/save_post/${bSeq}`);
 
     },
     /**
@@ -16,6 +19,9 @@ const boardLib = {
     */
     deleteSave(bSeq) {
 
+        const { ajaxLoad } = commonLib;
+
+        ajaxLoad('DELETE', `/api/board/save_post/${bSeq}`);
     }
 }
 
@@ -25,16 +31,17 @@ window.addEventListener("DOMContentLoaded", function() {
     for (const el of savePosts) {
         el.addEventListener("click", function() {
             // data-seq : 게시글 번호
+            // save_post에 on 클래스가 포함되어 있는 경우 -> 찜 한 상태, on -> 찜을 아직 안한 상태
             const bSeq = this.dataset.seq;
-
-            boardLib.save(bSeq);
-
+            if (this.classList.contains('on')) { // 찜한 상태 -> 해제
+                boardLib.deleteSave(bSeq);
+            } else { // 찜을 아직 안한 상태 -> 찜하기
+                boardLib.save(bSeq);
+            }
         });
     }
 
     /* 찜하기 처리 E */
 
-    /* 찜하기 해제 처리 S */
 
-    /* 찜하기 해제 처리 E */
 });
