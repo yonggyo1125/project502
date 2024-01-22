4c9b8d18eb9a50471ddb6b7ced92345b53f2ad24
yonggyo1125 <yonggyo00@kakao.com>
2024-01-22 15:57:24 +0900

    3조!

--- src/main/resources/static/front/js/board/view.js
+++ src/main/resources/static/front/js/board/view.js
@@ -14,11 +14,37 @@ window.addEventListener("DOMContentLoaded", function() {
         el.addEventListener("click", function() {
             const dataset = this.dataset;
             const seq = dataset.seq;
+            const commentEl = document.getElementById(`comment_${seq}`);
+            const targetEl = commentEl.querySelector(".comment");
+
+            /**
+            * targetEl의 하위 요소로 textarea가 있는 경우 : 댓글 수정 처리
+            * 없는 경우는 -> 비회원 비밀번호 검증 또는 texarea 노출 처리
+            */
+
+            const textarea = targetEl.querySelector("textarea");
+            if (textarea) { // 댓글 수정 처리
+                const content = textarea.value.trim();
+                const formData = new FormData();
+                formData.append('mode', 'edit');
+                formData.append('seq', seq);
+                formData.append('content', content);
+
+                const { ajaxLoad } = commonLib;
+
+                ajaxLoad('PATCH', '/api/comment', formData, 'json')
+                    .then(res => {
+                        targetEl.innerHTML = content.replace(/\n/gm, '<br>')
+                                                    .replace(/\r/gm, '');
+
+                    })
+                    .catch(err => console.error(err));
+
+                return;
+            }
+
 
             if (dataset.editable == 'false') { // 비회원 댓글 -> 비밀번호 확인 필요
-                const commentEl = document.getElementById(`comment_${seq}`);
-                const targetEl = commentEl.querySelector(".comment");
-
                 checkRequiredPassword(seq, () => callbackSuccess(seq), function() {
                     // 비번확인이 필요한 경우
                     const passwordBox = document.createElement("input");
